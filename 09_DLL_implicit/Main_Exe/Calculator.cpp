3384191c8f9b258c39796c808b291f6f8a46a573
Aditya Sihare <sihare.aditya@gmail.com>
2019-09-22 12:40:24 +0530

    DLL
    implicit

---
+++ 09_DLL_implicit/Main_Exe/Calculator.cpp
@@ -0,0 +1,19 @@
+
+#include "..\Win32Project1\math.h"
+#include <stdio.h>
+#include <windows.h>
+
+
+int main()
+{
+
+	int a=5, b=5, c;
+	
+	printf("ADD = %d\n", add(a, b, c));
+	printf("sub = %d\n", sub(a, b, c));
+	printf("mul = %d\n", mul(a, b, c));
+
+	return 0;
+
+}
+
